e067dfef3e69a10b70c41fd4b8ef9712e25b1aa1
Max <87779396+MaxiderZocker@users.noreply.github.com>
2025-01-27 13:42:12 +0100

    End of lesson

--- LottoDT112024/LottoDT112024/LottoDT112024.cpp
+++ LottoDT112024/LottoDT112024/LottoDT112024.cpp
@@ -5,19 +5,57 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	int zahl;
-	srand(time(NULL));
-	for (int j = 0; j < 12; j++) {
-		for (int i = 0; i < 6; i++) {
-			zahl = rand() % 49 + 1;
-			cout << zahl << "\t";
-		}
-		cout << endl;
-	}
-	
-	
-	
-	
+bool isInArray(int arr[], int size, int number) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == number) {
+            return true; // Zahl ist bereits im Array
+        }
+    }
+    return false; // Zahl ist noch nicht im Array
+}
+
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Vertausche Elemente
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int zahl[6];
+    srand(time(NULL));
+
+    for (int i = 0; i < 6; i++) {
+        int newNumber;
+        do {
+            newNumber = rand() % 49 + 1;
+        } while (isInArray(zahl, i, newNumber)); // Solange die Zahl schon drin ist, wiederhole
+        zahl[i] = newNumber;
+    }
+
+    // Ausgabe vor dem Sortieren
+    cout << "Unsortiert: ";
+    for (int i = 0; i < 6; i++) {
+        cout << zahl[i] << " ";
+    }
+    cout << endl;
+
+    // Sortieren mit Bubble Sort
+    bubbleSort(zahl, 6);
+
+    // Ausgabe nach dem Sortieren
+    cout << "Sortiert: ";
+    for (int i = 0; i < 6; i++) {
+        cout << zahl[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
