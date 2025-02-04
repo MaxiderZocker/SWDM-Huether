91377ed33dc59fc0a3e87c3e7c86a27617beab71
Max <87779396+MaxiderZocker@users.noreply.github.com>
2025-02-04 10:52:23 +0100

    End of Lesson

---
+++ DT11Taschenrechner/DT11Taschenrechner/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DT11Taschenrechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + ",";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + " + ";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + " - ";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + " * ";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = textBoxAnzeige.Text + " % ";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            String del = textBoxAnzeige.Text;
+            if (del.Length > 0)
+            {
+                textBoxAnzeige.Text = del.Remove(del.Length - 1);
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBoxAnzeige.Text = "";
+        }
+
+    }
+}
